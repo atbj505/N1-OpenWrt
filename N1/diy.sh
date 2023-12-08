70b51bc48efa47443132fdace69e68d25b1362fc
Robert <atbj505@hotmail.com>
2023-12-08 11:39:25 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -3,9 +3,4 @@
 # Default IP
 sed -i 's/192.168.1.1/192.168.3.100/g' package/base-files/files/bin/config_generate
 
-# Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall feeds/packages/net/haproxy feeds/packages/net/v2ray-geodata
-
-# Add packages
-svn co https://github.com/nantayo/my-pkg/trunk package/my-pkg
 svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
