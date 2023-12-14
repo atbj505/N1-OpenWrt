35ad0406c2acd79e2f4c7d553ce27ccdf5e71d14
Robert <atbj505@hotmail.com>
2023-12-14 22:53:53 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -4,6 +4,3 @@
 sed -i 's/192.168.1.1/192.168.3.100/g' package/base-files/files/bin/config_generate
 
 svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
-
-rm -rf package/helloworld
-git clone -b main --depth=1 https://github.com/fw876/helloworld.git package/helloworld
